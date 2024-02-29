acde1f737a2d4ddf6856853fc2cea45aad4a705a
christiandigiandomenico <christian.digiandomenico1990@gmail.com>
2024-02-29 15:50:37 +0100

    Content terminato

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <script>
 import AppNav from './components/AppNav.vue';
+import AppContent from './components/AppContent.vue';
 
 export default {
   components: {
     AppNav,
+    AppContent,
   }
 }
 </script>
 
 <template>
   <AppNav></AppNav>
+  <AppContent></AppContent>
 </template>
 
 <style></style>
---
+++ src/components/AppContent.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+    name: "AppContent",
+}
+</script>
+
+<template>
+    <div class="main-content">
+
+        <div class="text-content">
+            Content goes here
+        </div>
+
+    </div>
+</template>
+
+<style lang="scss">
+.main-content {
+    background-color: #1c1c1c;
+
+    .text-content {
+        font-size: 42px;
+        padding: 24px;
+        max-width: 1270px;
+        margin: auto;
+    }
+}
+</style>
