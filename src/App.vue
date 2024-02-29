661c92733dfe39feb894610736e2202240922b0a
christiandigiandomenico <christian.digiandomenico1990@gmail.com>
2024-02-29 16:11:47 +0100

    Merchandise terminato

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script>
 import AppNav from './components/AppNav.vue';
 import AppContent from './components/AppContent.vue';
+import AppMerchandise from './components/AppMerchandise.vue';
 
 export default {
   components: {
     AppNav,
     AppContent,
+    AppMerchandise,
   }
 }
 </script>
@@ -13,6 +15,7 @@ export default {
 <template>
   <AppNav></AppNav>
   <AppContent></AppContent>
+  <AppMerchandise></AppMerchandise>
 </template>
 
 <style></style>
---
+++ src/components/AppMerchandise.vue
@@ -0,0 +1,83 @@
+<script>
+export default {
+    name: "AppMerchandise",
+
+    data() {
+        return {
+            merchandise: [
+                {
+                    name: "DITIGAL COMICS",
+                    url: '/img/buy-comics-digital-comics.png'
+                },
+                {
+                    name: "DC MERCHANDISE",
+                    url: '/img/buy-comics-merchandise.png'
+                },
+                {
+                    name: "SUBSCRIPTION",
+                    url: '/img/buy-comics-subscriptions.png'
+                },
+                {
+                    name: "COMIC SHOP LOCATOR",
+                    url: '/img/buy-comics-shop-locator.png'
+                },
+                {
+                    name: "DC POWER VISA",
+                    url: '/img/buy-dc-power-visa.svg'
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="main-content-merchandise">
+
+        <div class="merchandise-content">
+
+            <ul>
+                <li v-for="merchandiseLinks in merchandise">
+                    <img :src="merchandiseLinks.url" alt="Merchandise Logo">
+                    {{ merchandiseLinks.name }}
+                </li>
+            </ul>
+
+        </div>
+
+    </div>
+</template>
+
+<style lang="scss">
+.main-content-merchandise {
+    background-color: #0282f9;
+
+    .merchandise-content {
+        padding-top: 50px;
+        padding-bottom: 50px;
+        max-width: 1270px;
+        margin: auto;
+
+        ul {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            gap: 50px;
+            list-style-type: none;
+
+            li {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                gap: 10px;
+
+                img {
+                    object-fit: contain;
+                    width: 50px;
+                    height: 50px;
+                }
+            }
+        }
+    }
+}
+</style>
