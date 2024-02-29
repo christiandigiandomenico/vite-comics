439abcc22b6a42c2c93da0519d08e22320b61fdd
christiandigiandomenico <christian.digiandomenico1990@gmail.com>
2024-02-29 15:35:47 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+import AppNav from './components/AppNav.vue';
+
+export default {
+  components: {
+    AppNav,
+  }
+}
+</script>
+
+<template>
+  <AppNav></AppNav>
+</template>
+
+<style></style>
---
+++ src/components/AppNav.vue
@@ -0,0 +1,61 @@
+<script>
+export default {
+    name: "AppNav",
+
+    data() {
+        return {
+            links: ["Characters", "Comics", "Movies", "Tv", "Games", "Collectibles", "Videos", "Fans", "News", "Shop"],
+
+            activeLinkIndex: 1,
+        }
+    }
+}
+</script>
+
+<template>
+    <nav>
+
+        <div class="nav-content">
+
+            <div class="logo">
+                <img src="/img/dc-logo.png" alt="DC logo">
+            </div>
+
+            <div class="nav-links">
+
+                <ul>
+                    <li v-for="activeLinks in links">
+                        {{ activeLinks }}
+                    </li>
+                </ul>
+
+            </div>
+
+        </div>
+
+    </nav>
+</template>
+
+<style lang="scss">
+nav {
+    background-color: white;
+    color: black;
+
+    .nav-content {
+        display: flex;
+        max-width: 1270px;
+    }
+
+    .logo img {
+        width: 75px;
+        height: 75px;
+    }
+
+    .nav-links {
+        ul {
+            list-style-type: none;
+            display: flex;
+        }
+    }
+}
+</style>
