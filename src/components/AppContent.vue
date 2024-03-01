d03ce26a7b2fffac2e59826893f95a96f8d4437a
christiandigiandomenico <christian.digiandomenico1990@gmail.com>
2024-03-01 15:52:31 +0100

    Aggiunta elementi comics

--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -1,10 +1,92 @@
 <script>
 import AppMerchandise from './AppMerchandise.vue';
+import ComicItem from './ComicItem.vue';
 
 export default {
     name: "AppContent",
     components: {
         AppMerchandise,
+        ComicItem,
+    },
+
+    data() {
+        return {
+
+            comics: [
+                {
+                    "thumb": "https://www.coverbrowser.com/image/action-comics/1-1.jpg",
+                    "price": "$19.99",
+                    "series": "Action Comics",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg",
+                    "price": "$3.99",
+                    "series": "American Vampire 1976",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg",
+                    "price": "$16.99",
+                    "series": "Aquaman",
+                    "type": "graphic novel",
+                },
+                {
+                    "thumb": "https://d29xot63vimef3.cloudfront.net/image/batgirl/1-1.jpg",
+                    "price": "$2.99",
+                    "series": "Batgirl",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://static.posters.cz/image/750/locandine-film-in-plexiglass-batman-prowl-comic-cover-i69653.jpg",
+                    "price": "$3.99",
+                    "series": "Batman",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/5/50/Batman_Beyond_v.1_1.jpg",
+                    "price": "$2.99",
+                    "series": "Batman Beyond",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/0/0d/Batman_Superman_Vol_1_1.jpg",
+                    "price": "$3.99",
+                    "series": "Batman/Superman",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/c/cf/Batman_Superman_Annual_Vol_2_1.jpg",
+                    "price": "$4.99",
+                    "series": "Batman/Superman Annual",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/5/54/Batman_The_Joker_War_Zone_Vol_1_1.jpg",
+                    "price": "$5.99",
+                    "series": "Batman: The Joker War Zone",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/6/64/Batman_Three_Jokers_Collected.jpg",
+                    "price": "$6.99",
+                    "series": "Batman: Three Jokers",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/f/f8/Batman_White_Knight_Presents_Harley_Quinn_Vol_1_1.jpg",
+                    "price": "$4.99",
+                    "series": "Batman: White Knight Presents: Harley Quinn",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/c/c8/Catwoman_Vol_2_1.jpg",
+                    "price": "$16.99",
+                    "series": "Catwoman",
+                    "type": "graphic novel",
+                },
+            ]
+        }
     }
 }
 </script>
@@ -12,8 +94,22 @@ export default {
 <template>
     <div class="main-content">
 
+        <img src="/img/jumbotron.jpg" alt="DC Jumbotron">
+
+        <div class="current-series">
+
+            CURRENT SERIES
+
+        </div>
+
         <div class="text-content">
-            Content goes here
+
+            <ComicItem v-for="comic in comics" :comicName="comic.series" :comicImage="comic.thumb"></ComicItem>
+
+        </div>
+
+        <div class="learn-more">
+            <button>Load More</button>
         </div>
 
     </div>
@@ -21,14 +117,53 @@ export default {
 </template>
 
 <style lang="scss">
+@use '../styles/variables' as *;
+
 .main-content {
     background-color: #1c1c1c;
+    position: relative;
+
+    .current-series {
+        background-color: $primaryColor;
+        width: 200px;
+        text-align: center;
+        padding: 10px;
+        font-size: 20px;
+        font-weight: bold;
+
+        position: absolute;
+        top: 380px;
+        left: 300px;
+    }
+
+    img {
+        height: 400px;
+        width: 100vw;
+        object-fit: cover;
+        object-position: top;
+    }
 
     .text-content {
         font-size: 42px;
         padding: 24px;
         max-width: 1270px;
         margin: auto;
+        display: flex;
+        flex-wrap: wrap;
+        margin-top: 20px;
+    }
+
+    .learn-more {
+        text-align: center;
+
+        button {
+            border: none;
+            background-color: $primaryColor;
+            padding: 10px 40px;
+            font-size: 12px;
+            margin-bottom: 20px;
+            font-weight: bold;
+        }
     }
 }
 </style>
---
+++ src/components/ComicItem.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+    name: 'ComicItem',
+    props: {
+        comicName: String,
+        comicImage: String
+    },
+}
+</script>
+
+<template>
+    <div class="comic">
+        <img :src="comicImage" :alt="comicName">
+        <h4>{{ comicName }}</h4>
+    </div>
+</template>
+
+<style lang="scss">
+.comic {
+    width: calc(100% / 6);
+
+    img {
+        width: 100px;
+        height: 100px;
+    }
+
+    h4 {
+        font-size: 14px;
+        font-weight: 400;
+    }
+}
+</style>
